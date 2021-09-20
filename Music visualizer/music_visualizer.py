b4afbafb6baf96cf3657d62857f424b48a2cb4e0
Murphy <git@murphy-in.space>
2021-09-20 11:30:08 +0200

    include draw function

--- Music visualizer/music_visualizer.py
+++ Music visualizer/music_visualizer.py
@@ -11,79 +11,94 @@ CENTER_X = SCREEN_WIDTH/2
 CENTER_Y = SCREEN_HEIGHT/2
 
 SONG = "source/INDUSTRY_BABY_feat_Jack_Harlow.wav"  # put your song here
-INDUSTRY_BABY = arcade.Sound(SONG)
 AUDIO_TIME_SERIES, SAMPLING_RATE = librosa.load(SONG)
 
-FPS = 80
+# sampling rate for new data for industry baby is 40 fps
+FPS = 40
 
 
 class visualizer(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, update_rate=1/FPS)
-
         arcade.set_background_color(arcade.csscolor.WHITE)
-        self.frameNumber = 0
-        self.player = None
         self.color = arcade.csscolor.LIME
-        self.part = 0
+
+        # sound stuff
+        self.song = arcade.Sound(SONG)
+        self.player = None
+
+        # logic stuff
+        self.frameNumber = 0
+        self.part = 0  # part of the audio time series that the song is currently at
+        self.pause = False
+
+        self.value = 0  # the value at this part
+        self.points = []  # points for the line strip graph or the point graph
 
     def setup(self):
-        # AUDIO_TIME_SERIES_ABS = np.abs(AUDIO_TIME_SERIES)
-        self.points = []
-        self.line_points = []
         pass
 
-    def on_draw(self):
-        pass
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.SPACE:
+            if self.pause:
+                self.song.play(loop=True)
+            else:
+                self.song.stop(self.player)
+            self.pause = not self.pause
 
     def on_update(self, delta_time: float):
-        self.part = int(INDUSTRY_BABY.get_stream_position(self.player) * SAMPLING_RATE)
+        if not self.pause:
+            self.part = int(self.song.get_stream_position(self.player) * SAMPLING_RATE)
 
+            self.update_points()
+            # self.update_single_value()  # for the circle, line and rect
+
+    def on_draw(self):
         arcade.start_render()  # ---
-        self.graph_line_strip(AUDIO_TIME_SERIES[self.part])
+
+        self.draw_graph_line_strip()
+        # self.draw_graph_points()
+        # self.draw_circle()
+        # self.draw_line()
+        # self.draw_rect()
+
         arcade.finish_render()  # ---
 
-    def graph_line_strip(self, value):
-        self.line_points.append([SCREEN_WIDTH, CENTER_Y + value * (CENTER_Y - 100)])
-        arcade.draw_line_strip(self.line_points, color=self.color, line_width=2)
-
-        self.line_points = np.array(self.line_points)
-        self.line_points[:, 0] = self.line_points[:, 0] - 2
-
-        self.line_points = np.delete(self.line_points, np.where(self.line_points[:, 0] <= 0), axis=0)
-
-        self.line_points = self.line_points.tolist()
-
-    def graph_points(self, value):
-        self.points.append([SCREEN_WIDTH, CENTER_Y + value * (CENTER_Y - 100)])
-        arcade.draw_points(self.points, color=self.color, size=3)
+    def update_points(self):
+        self.points.append([SCREEN_WIDTH, CENTER_Y + AUDIO_TIME_SERIES[self.part] * (CENTER_Y - 100)])
 
         self.points = np.array(self.points)
-        self.points[:, 0] = self.points[:, 0] - 4  # shift all the points to the left
+        self.points[:, 0] = self.points[:, 0] - 3  # shift all the points to the left
 
-        self.points = np.delete(self.points, np.where(self.points[:, 0] <= 0), axis=0)  # delete all the points that are outside of the window
+        # delete all the points that are outside of the window
+        self.points = np.delete(self.points, np.where(self.points[:, 0] <= 0), axis=0)
         self.points = self.points.tolist()
 
-    def rect(self, value):
-        arcade.draw_rectangle_filled(
-            CENTER_X, CENTER_Y, 800 * value, 100, color=self.color)
+    def update_single_value(self):
+        self.value = abs(AUDIO_TIME_SERIES[self.part])
 
-    def line(self, value):
-        arcade.draw_line(CENTER_X, CENTER_Y, CENTER_X + 400 * value,
-                         CENTER_Y, line_width=8, color=self.color)
-        arcade.draw_line(CENTER_X, CENTER_Y, CENTER_X - 400 * value,
-                         CENTER_Y, line_width=8, color=self.color)
+    def draw_graph_line_strip(self):
+        arcade.draw_line_strip(self.points, color=self.color, line_width=2)
 
-    def circle(self, value):
-        arcade.draw_circle_outline(
-            CENTER_X, CENTER_Y, 250 * value, color=self.color, border_width=5)
+    def draw_graph_points(self):
+        arcade.draw_points(self.points, color=self.color, size=3)
+
+    def draw_rect(self):
+        arcade.draw_rectangle_filled(CENTER_X, CENTER_Y, 800 * self.value, 100, color=self.color)
+
+    def draw_line(self):
+        arcade.draw_line(CENTER_X, CENTER_Y, CENTER_X + 400 * self.value, CENTER_Y, line_width=8, color=self.color)
+        arcade.draw_line(CENTER_X, CENTER_Y, CENTER_X - 400 * self.value, CENTER_Y, line_width=8, color=self.color)
+
+    def draw_circle(self):
+        arcade.draw_circle_outline(CENTER_X, CENTER_Y, 250 * self.value, color=self.color, border_width=5)
 
 
 def main():
     window = visualizer()
     window.setup()
 
-    window.player = INDUSTRY_BABY.play()
+    window.player = window.song.play(loop=True)
     arcade.run()
 
 
