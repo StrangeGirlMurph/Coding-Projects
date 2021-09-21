b0e7413b512f26302ef782c9995a41e0c2c1e12a
NanoKaano <kaansuennenwold@gmail.com>
2021-09-21 12:58:11 +0200

    stable

--- Music visualizer/librosa_playground.py
+++ Music visualizer/librosa_playground.py
@@ -1,22 +1,26 @@
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter
+from scipy.signal import savgol_filter, resample
 import numpy as np
 
 
 def main():
     y, sr = librosa.load(
-        "media/INDUSTRY_BABY_feat_Jack_Harlow.wav")
+        "source/INDUSTRY_BABY_feat_Jack_Harlow.wav")
     print(y)
     print(sr)
     print(len(y))
 
+    downrate = resample(y, int((len(y)/sr)*100))
+
     abs = np.abs(y)
-    smooth = savgol_filter(abs, window_length=99, polyorder=8)
+
+    #smooth = savgol_filter(abs, window_length=2001, polyorder=3)
 
     librosa.display.waveshow(abs, sr=sr)
-    librosa.display.waveshow(smooth, sr=sr)
+    # librosa.display.waveshow(smooth[0:100000], sr=sr)  # smoother.smooth_data[0]
+    librosa.display.waveshow(downrate, sr=100)
     plt.show()
 
 
--- Music visualizer/music_visualizer.py
+++ Music visualizer/music_visualizer.py
@@ -1,26 +1,35 @@
 # multiline comments Crtl + K + C (undo + U)
 
 import arcade
+from arcade.gl.enums import LINE_STRIP
 import librosa
 import librosa.display
 import numpy as np
+from scipy.signal import savgol_filter, resample
 
 # Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
 SCREEN_TITLE = "music visualizer"
 CENTER_X = SCREEN_WIDTH/2
 CENTER_Y = SCREEN_HEIGHT/2
 
+# options
+RECTANGLE = "rect"
+CIRCLE = "circle"
+CIRCLE_OUTLINE = "circle_outline"
+LINE = "line"
+POINT_GRAPH = "point_graph"
+LINE_GRAPH = "line_graph"
+
 SONG = "source/INDUSTRY_BABY_feat_Jack_Harlow.wav"  # put your song here
 AUDIO_TIME_SERIES, SAMPLING_RATE = librosa.load(SONG)
 
-# sampling rate for new data for industry baby is 40 fps
-FPS = 30
+FPS = 75  # (refresh rate of the visualizer)
 
 
 class Visualizer(arcade.Window):
-    def __init__(self):
+    def __init__(self, default):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, update_rate=1/FPS)
         arcade.set_background_color(arcade.csscolor.WHITE)
         self.color = arcade.csscolor.LIME
@@ -33,15 +42,15 @@ class Visualizer(arcade.Window):
         self.frameNumber = 0
         self.part = 0  # part of the audio time series that the song is currently at
         self.pause = False
+        self.option = default
 
         self.value = 0  # the value at this part
         self.points = []  # points for the line strip graph or the point graph
 
     def setup(self):
-        pass
+        self.smooth = savgol_filter(abs(AUDIO_TIME_SERIES), window_length=2001, polyorder=3)
 
     def on_key_press(self, key, modifiers):
-
         # pause and resume
         if key == arcade.key.SPACE:
             if not self.pause:
@@ -55,37 +64,63 @@ class Visualizer(arcade.Window):
             arcade.close_window()
             # arcade.exit()
 
+        # changing visualizer
+        if key == arcade.key.R:
+            self.option = RECTANGLE
+        elif key == arcade.key.C:
+            self.option = CIRCLE
+        elif key == arcade.key.O:
+            self.option = CIRCLE_OUTLINE
+        elif key == arcade.key.L:
+            self.option = LINE
+        elif key == arcade.key.P:
+            self.option = POINT_GRAPH
+        elif key == arcade.key.G:
+            self.option = LINE_GRAPH
+
+    #
     def on_update(self, delta_time):
         if not self.pause:
             self.part = int(self.song.get_stream_position(self.player) * SAMPLING_RATE)
 
-            # self.update_points()
-            self.update_single_value()  # for the circle, line and rect
+            if self.option in (POINT_GRAPH, LINE_GRAPH):
+                self.update_points(self.smooth * 2)
+            elif self.option in (RECTANGLE, CIRCLE, CIRCLE_OUTLINE, LINE):
+                self.update_single_value(self.smooth * 2)
 
     def on_draw(self):
         arcade.start_render()  # ---
 
-        # self.draw_graph_line_strip()
-        # self.draw_graph_points()
-        # self.draw_circle()
-        # self.draw_line()
-        self.draw_rect()
+        if self.option == RECTANGLE:
+            self.draw_rect()
+        elif self.option == CIRCLE:
+            self.draw_circle()
+        elif self.option == CIRCLE_OUTLINE:
+            self.draw_circle_outline()
+        elif self.option == LINE:
+            self.draw_line()
+        elif self.option == POINT_GRAPH:
+            self.draw_graph_points()
+        elif self.option == LINE_GRAPH:
+            self.draw_graph_line_strip()
 
         arcade.finish_render()  # ---
 
-    def update_points(self):
-        self.points.append([SCREEN_WIDTH, CENTER_Y + AUDIO_TIME_SERIES[self.part] * (CENTER_Y - 100)])
+    # update functions
+    def update_points(self, audio_signal):
+        self.points.append([SCREEN_WIDTH, CENTER_Y - CENTER_Y/2 + audio_signal[self.part] * CENTER_Y])
 
         self.points = np.array(self.points)
-        self.points[:, 0] = self.points[:, 0] - 3  # shift all the points to the left
+        self.points[:, 0] = self.points[:, 0] - 2  # shift all the points to the left
 
         # delete all the points that are outside of the window
-        self.points = np.delete(self.points, np.where(self.points[:, 0] <= 0), axis=0)
+        self.points = np.delete(self.points, np.where(self.points[:, 0] < 0), axis=0)
         self.points = self.points.tolist()
 
-    def update_single_value(self):
-        self.value = abs(AUDIO_TIME_SERIES[self.part])
+    def update_single_value(self, audio_signal):
+        self.value = abs(audio_signal[self.part])
 
+    # draw functions
     def draw_graph_line_strip(self):
         arcade.draw_line_strip(self.points, color=self.color, line_width=2)
 
@@ -99,17 +134,21 @@ class Visualizer(arcade.Window):
         arcade.draw_line(CENTER_X, CENTER_Y, CENTER_X + 400 * self.value, CENTER_Y, line_width=8, color=self.color)
         arcade.draw_line(CENTER_X, CENTER_Y, CENTER_X - 400 * self.value, CENTER_Y, line_width=8, color=self.color)
 
-    def draw_circle(self):
+    def draw_circle_outline(self):
         arcade.draw_circle_outline(CENTER_X, CENTER_Y, 250 * self.value, color=self.color, border_width=5)
 
+    def draw_circle(self):
+        arcade.draw_circle_filled(CENTER_X, CENTER_Y, 250 * self.value, color=self.color)
+
+    # sound
     def play_song(self):
         self.player = self.song.play(loop=True)
 
 
 def main():
-    music_visualizer = Visualizer()
-    music_visualizer.setup()
+    music_visualizer = Visualizer(default=RECTANGLE)
 
+    music_visualizer.setup()
     music_visualizer.play_song()
     arcade.run()
 
