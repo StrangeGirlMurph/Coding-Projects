505d4abe137ba576b82ffb670de7a83cde7f28c5
Murphy <git@murphy-in.space>
2021-09-22 15:36:15 +0200

    changing shapes prototype

--- Music visualizer/music_visualizer.py
+++ Music visualizer/music_visualizer.py
@@ -15,6 +15,7 @@ LINE = "line"
 POINT_GRAPH = "point_graph"
 LINE_GRAPH = "line_graph"
 KLEIN_BOTTLE = "klein_bottle"
+CHANGING_SHAPES = "changing_shapes"
 
 # Constants
 DEFAULT_WIDTH = 1100
@@ -27,8 +28,17 @@ KLEIN_BOTTLE_SPRITE = arcade.Sprite("media/Klein_bottle.png", scale=1, center_x=
 # sound
 SONG = "source/INDUSTRY_BABY_feat_Jack_Harlow.wav"  # put your song here
 AUDIO_TIME_SERIES, SAMPLING_RATE = librosa.load(SONG)
+TEMPO, BEAT_FRAMES = librosa.beat.beat_track(y=AUDIO_TIME_SERIES, sr=SAMPLING_RATE)
+# BEATS = librosa.frames_to_time(BEAT_FRAMES, sr=SAMPLING_RATE)
 
 FPS = 75  # (refresh rate of the visualizer)
+
+# how to add a new visualizer:
+# 1. create a constant for it under options
+# 2. add a key in on_key_press()
+# 3. add it to one of the on_update options
+# 4. create a draw function and add that function to the draw function
+# 5. edit the readme
 
 
 class Visualizer(arcade.Window):
@@ -47,6 +57,7 @@ class Visualizer(arcade.Window):
 
         # logic stuff
         self.frameNumber = 0
+        self.cycle = 0  # for changing shapes
         self.part = 0  # part of the audio time series that the song is currently at
         self.pause = False
         self.option = default
@@ -55,7 +66,7 @@ class Visualizer(arcade.Window):
         self.points = []  # points for the line strip graph or the point graph
 
     def setup(self):
-        self.smooth = savgol_filter(abs(AUDIO_TIME_SERIES), window_length=2001, polyorder=3)
+        self.smooth = savgol_filter(abs(AUDIO_TIME_SERIES), window_length=2001, polyorder=3) * 2
 
     def on_resize(self, width, height):
         super().on_resize(width, height)
@@ -73,6 +84,7 @@ class Visualizer(arcade.Window):
             else:
                 self.player.play()
             self.pause = not self.pause
+            return
 
         # close
         if key == arcade.key.ESCAPE:
@@ -96,16 +108,22 @@ class Visualizer(arcade.Window):
             self.option = LINE_GRAPH
         elif key == arcade.key.K:
             self.option = KLEIN_BOTTLE
+        elif key == arcade.key.S:
+            self.option = CHANGING_SHAPES
+        else:
+            return
+        print(f'Choosen visualizer: {self.option}')
 
-    #
     def on_update(self, delta_time):
         if not self.pause:
             self.part = int(self.song.get_stream_position(self.player) * SAMPLING_RATE)
 
             if self.option in (POINT_GRAPH, LINE_GRAPH):
-                self.update_points(self.smooth * 2)
-            elif self.option in (RECTANGLE, CIRCLE, CIRCLE_OUTLINE, LINE, KLEIN_BOTTLE):
-                self.update_single_value(self.smooth * 2)
+                self.update_points(self.smooth)
+            elif self.option in (RECTANGLE, CIRCLE, CIRCLE_OUTLINE, LINE, KLEIN_BOTTLE, CHANGING_SHAPES):
+                self.update_single_value(self.smooth)
+
+            self.frameNumber += 1
 
     def on_draw(self):
         arcade.start_render()  # ---
@@ -124,6 +142,8 @@ class Visualizer(arcade.Window):
             self.draw_graph_line_strip()
         elif self.option == KLEIN_BOTTLE:
             self.draw_klein_bottle()
+        elif self.option == CHANGING_SHAPES:
+            self.draw_changing_shapes()
 
         arcade.finish_render()  # ---
 
@@ -166,6 +186,28 @@ class Visualizer(arcade.Window):
         KLEIN_BOTTLE_SPRITE.draw()
         # sound
 
+    def draw_changing_shapes(self):
+        # sensitivity = 1000
+        # if self.part < BEAT_FRAMES[np.where(BEAT_FRAMES < self.part)][-1] + sensitivity:
+        #     self.cycle += 1
+        #     print("next shape")
+
+        if self.frameNumber % 20 <= .5 and self.value >= 0.8:
+            self.cycle += 1
+
+        # reset if necessary
+        if self.cycle == 3:
+            self.cycle = 0
+
+        # drawing
+        if self.cycle == 0:
+            self.draw_rect()
+        elif self.cycle == 1:
+            self.draw_circle()
+        elif self.cycle == 2:
+            self.draw_line()
+
+    # sound stuff
     def play_song(self):
         self.player = self.song.play(loop=True)
 
