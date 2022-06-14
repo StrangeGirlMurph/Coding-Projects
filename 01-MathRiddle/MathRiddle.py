7db5c1167fe18e9d1068b49a600a9d92c295afe9
Murphy <git@murphy-in.space>
2022-06-14 10:37:17 +0200

    update

--- 01-MathRiddle/MathRiddle.py
+++ 01-MathRiddle/MathRiddle.py
@@ -1,12 +1,7 @@
 import numpy as np
 
-
-def getListOfPrimes(n):
-    primes = np.arange(3, n+1, 2)
-    for i in primes:
-        primes = list(filter(lambda x: x % i != 0 or i == x, primes))
-    return [2] + primes
-
-
-primes = getListOfPrimes(50)
-print(primes)
+pairsTo50 = [(x, y) for x in range(2, 51) for y in range(2, 51)]  # Generate all pairs of numbers from 2 to 50 with order
+primesTo50 = [x for x in range(2, 51) if all(x % y != 0 for y in range(2, x))]
+antiPrimesTo50 = [x for x in range(2, 51) if not all(x % y != 0 for y in range(2, x))]
+allSums = np.unique([x + y for (x, y) in pairsTo50])
+allProducts = np.unique([x * y for (x, y) in pairsTo50])
