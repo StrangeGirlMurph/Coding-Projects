6db90eaa23d2413874320d3b6763e0d971889f93
Murphy <git@murphy-in.space>
2022-11-19 11:02:07 +0100

    merge conflict

--- 07-AdventOfCode2021/15/day-15.py
+++ 07-AdventOfCode2021/15/day-15.py
@@ -3,54 +3,6 @@ import numpy as np
 with open('input.txt', 'r') as f:
     lines = f.readlines()
 lines = np.array([list(line[:-1]) for line in lines])
-<<<<<<< HEAD
-baseGrid = lines.astype(np.uint16)
-
-baseGrid[0][0] = 0  # dont visit the start again
-
-print(baseGrid)
-
-# distances = np.zeros((100, 100))
-# for (x, y), val in np.ndenumerate(distances):
-#     distances[x][y] = x + y
-
-# distances = distances[::-1, ::-1]  # flip
-# print(distances)
-
-bestScore = float("inf")
-
-
-def searchPath(grid, posy, posx, currentscore):
-    # search the adjacent sides recursively
-    if posx + posy == 200:  # reached the end
-        global bestScore
-        if currentscore < bestScore:
-            bestScore = currentscore
-    else:
-        # go through all neighbors
-        for (dy, dx), val in np.ndenumerate(grid[posy-1:posy+2, posx-1:posx+2]):
-
-            # if legal step (not already visited (val != 0) + (dy+dx) % 2 != 0 filters the diagonals)
-            # dy+dx
-            # 0 1 2
-            # 1 2 3
-            # 2 3 4
-            if val != 0 and (dy+dx) % 2 != 0:
-                newy = posy - 1 + dy
-                newx = posx - 1 + dx
-
-                currentscore += val
-
-                tempGrid = grid
-                tempGrid[newy][newx] = 0
-                searchPath(tempGrid, newy, newx)
-
-
-startingGrid = np.pad(baseGrid, 1, mode="constant", constant_values=0)
-searchPath(startingGrid, posy=1, posx=1, currentscore=0)
-
-print(bestScore)
-=======
 baseGrid = lines.astype(np.float32)
 size = len(baseGrid)
 
@@ -174,4 +126,3 @@ for (dy, dx), val in np.ndenumerate(np.zeros((5, 5))):
 Grid2[Grid2 == 0] = 9
 Grid2[0][0] = 0
 findCheapestPath(Grid2)
->>>>>>> d799cf755a9d999e18230c9724332022f5601f15
