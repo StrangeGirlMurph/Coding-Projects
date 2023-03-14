541e8498a86d2e7d8e753b04a7deeb5a713a7e5e
Murphy <git@murphy-in.space>
2023-03-14 17:15:56 +0100

    leibniz formula

--- 12-PiDay2023/src/main.rs
+++ 12-PiDay2023/src/main.rs
@@ -1,26 +1,15 @@
-use rand::prelude::*;
+use indicatif::ProgressIterator;
 
 fn main() {
-    let end = 1000;
-    let mut pi: f64;
-    let mut rng = rand::thread_rng();
-    let mut coprime_count = 1;
+    let mut pi: u128 = 0;
+    let digits = 1000000000000;
 
-    println!("Let's get this party started!");
-
-    for i in 1.. {
-        if are_coprime(rng.gen_range(1..end), rng.gen_range(1..end)) {
-            coprime_count += 1;
+    for i in (0..1000000000u32).progress() {
+        if i & 1 == 1 {
+            pi = pi - 4 * digits / (2 * i + 1) as u128;
+        } else {
+            pi = pi + 4 * digits / (2 * i + 1) as u128;
         }
-
-        pi = (6.0 / (coprime_count as f64 / i as f64)).sqrt();
-        println!("{} \t  {}", pi, i);
     }
-}
-
-fn are_coprime(mut a: i32, mut b: i32) -> bool {
-    while b != 0 {
-        (a, b) = (b, a % b);
-    }
-    a == 1
+    println!("{}", pi);
 }
