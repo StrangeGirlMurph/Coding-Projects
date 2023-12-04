9e6d595dca7bfbd64586b3fc9afdd346cfd211a4
Murphy <git@murphy-in.space>
2023-12-04 09:37:06 +0100

    day4 :)

--- AdventOfCode/aoc-2023-rust/src/bin/day04.rs
+++ AdventOfCode/aoc-2023-rust/src/bin/day04.rs
@@ -1,11 +1,74 @@
 use std::fs;
 
+use regex::Regex;
+
 pub fn day4_part1(input: &str) -> usize {
-    input.lines().count()
+    let re = Regex::new(r"\d+").unwrap();
+
+    input
+        .lines()
+        .map(|l| {
+            let split: Vec<&str> = l.split(": ").nth(1).unwrap().split(" | ").collect();
+            let winning_numbers = re
+                .find_iter(split[0])
+                .map(|m| m.as_str().parse().unwrap())
+                .collect::<Vec<usize>>();
+            let numbers = re
+                .find_iter(split[1])
+                .map(|m| m.as_str().parse().unwrap())
+                .collect::<Vec<usize>>();
+
+            let mut number_of_winners = 0;
+            for number in numbers {
+                if winning_numbers.contains(&number) {
+                    number_of_winners += 1;
+                }
+            }
+
+            if number_of_winners == 0 {
+                0
+            } else {
+                2usize.pow(number_of_winners - 1)
+            }
+        })
+        .sum()
 }
 
 pub fn day4_part2(input: &str) -> usize {
-    input.lines().count()
+    let re = Regex::new(r"\d+").unwrap();
+
+    let winners_per_game = input
+        .lines()
+        .map(|l| {
+            let split: Vec<&str> = l.split(": ").nth(1).unwrap().split(" | ").collect();
+            let winning_numbers = re
+                .find_iter(split[0])
+                .map(|m| m.as_str().parse().unwrap())
+                .collect::<Vec<usize>>();
+            let numbers = re
+                .find_iter(split[1])
+                .map(|m| m.as_str().parse().unwrap())
+                .collect::<Vec<usize>>();
+
+            let mut number_of_winners = 0;
+            for number in numbers {
+                if winning_numbers.contains(&number) {
+                    number_of_winners += 1;
+                }
+            }
+
+            number_of_winners
+        })
+        .collect::<Vec<usize>>();
+
+    let mut copies: Vec<usize> = vec![1; winners_per_game.len()];
+    for i in 0..copies.len() {
+        for j in (i + 1)..=(i + winners_per_game[i]) {
+            copies[j] += copies[i];
+        }
+    }
+
+    copies.iter().sum()
 }
 
 fn main() {
