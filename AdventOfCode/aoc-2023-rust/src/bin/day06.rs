ad49bbfa4714d65d508fcd0110febe3d5885d65c
Murphy <git@murphy-in.space>
2023-12-06 10:31:40 +0100

    day6

--- AdventOfCode/aoc-2023-rust/src/bin/day06.rs
+++ AdventOfCode/aoc-2023-rust/src/bin/day06.rs
@@ -1,15 +1,32 @@
-use std::fs;
-
-pub fn day6_part1(input: &str) -> usize {
-    input.lines().count()
+pub fn day6_part1() -> usize {
+    [(53, 313), (89, 1090), (76, 1214), (98, 1201)]
+        .iter()
+        .map(|race| {
+            let mut num = 0;
+            for t in 0..=race.0 {
+                if race.1 < (race.0 - t) * t {
+                    num += 1;
+                }
+            }
+            num
+        })
+        .product()
 }
 
-pub fn day6_part2(input: &str) -> usize {
-    input.lines().count()
+pub fn day6_part2() -> usize {
+    let time = 53897698;
+    let distance: u64 = 313109012141201;
+
+    let mut num = 0;
+    for t in 0..=time {
+        if distance < (time - t) * t {
+            num += 1;
+        }
+    }
+    num
 }
 
 fn main() {
-    let input = fs::read_to_string("input/2023/day6.txt").unwrap();
-    println!("Day 6, Part 1: {}", day6_part1(&input));
-    println!("Day 6, Part 2: {}", day6_part2(&input));
+    println!("Day 6, Part 1: {}", day6_part1());
+    println!("Day 6, Part 2: {}", day6_part2());
 }
