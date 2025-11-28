0e740c0689e71267132874ffa7e7a95cdc7e9709
Murphy Sünnenwold <git@murphy.science>
2025-11-28 18:46:19 +0100

    remove import

--- AdventOfCode/aoc-2025-deno/setup.ts
+++ AdventOfCode/aoc-2025-deno/setup.ts
@@ -1,8 +1,6 @@
-import open from "@rdsq/open";
-
 const day = (Deno.readDirSync(".").filter(v => v.name.startsWith("day") && v.name.endsWith("ts")).map(v => parseInt(v.name.slice(3,5))).toArray().sort().at(-1) || 0) + 1
 
-open(`https://adventofcode.com/2024/day/${day}`)
+new Deno.Command("xdg-open", {args: [`https://adventofcode.com/2024/day/${day}`]}).output()
 
 const response = await fetch(`https://adventofcode.com/2024/day/${day}/input`, {
     headers: {
