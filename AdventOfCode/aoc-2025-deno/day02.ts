b15c6fbf07f03baedcdc4fc1e34ff71421b5a16f
Murphy Sünnenwold <git@murphy.science>
2025-12-02 10:15:18 +0100

    day2

---
+++ AdventOfCode/aoc-2025-deno/day02.ts
@@ -0,0 +1,31 @@
+const input = Deno.readTextFileSync("./day02.txt").trim().split(",").map(s => s.split("-").map(t => parseInt(t))) as [number, number][]
+
+let sum = 0;
+
+for (const range of input) {
+    const start = range[0]
+    const end = range[1]
+
+    for (let i = start; i <= end; i++) {
+        const s = i.toString()
+        const len = s.length
+        if (len % 2 != 0) continue
+        else if (s.slice(0, len/2) == s.slice(len/2)) sum += i
+    }
+}
+
+console.log(sum)
+
+sum = 0;
+
+for (const range of input) {
+    const start = range[0]
+    const end = range[1]
+
+    for (let i = start; i <= end; i++) {
+        const s = i.toString()
+        if (/^([1-9]\d*)\1+$/.test(s)) sum += i
+    }
+}
+
+console.log(sum)
